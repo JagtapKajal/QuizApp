1b61440a0a7e4f53c7b0af1fe0dbb837ef83a691
jagtapK <jagtapkajal54@gmail.com>
2025-09-10 15:16:19 +0530

    Implemented Method to add Quiz data

---
+++ src/main/java/com/quizApp/quizApp/serviceImpl/QuizServiceImpl.java
@@ -0,0 +1,53 @@
+package com.quizApp.quizApp.serviceImpl;
+
+import com.quizApp.quizApp.DTO.QuestionDTO;
+import com.quizApp.quizApp.entity.Questions;
+import com.quizApp.quizApp.entity.Quiz;
+import com.quizApp.quizApp.repository.QuestionsRepository;
+import com.quizApp.quizApp.repository.QuizRepository;
+import com.quizApp.quizApp.service.QuizService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class QuizServiceImpl implements QuizService {
+
+    @Autowired
+    private QuizRepository quizRepository;
+
+    @Autowired
+    private QuestionsRepository questionsRepository;
+
+    // Method to create Quiz
+    @Override
+    public String createQuiz(String quizname, int noOfQuestions, String category) {
+
+        Quiz quiz = new Quiz();
+        quiz.setQuiztitle(quizname);
+        List<Questions> questionsList = quizRepository.findRandomQuestions(noOfQuestions, category);
+        quiz.setQuestionsList(questionsList);
+        quizRepository.save(quiz);
+        String msg = "Quiz created..";
+        return msg;
+    }
+
+
+    // Method to get Quiz questions
+    @Override
+    public List<QuestionDTO> getQuizQuestion(int id) {
+
+       Quiz quiz = quizRepository.findById(id).orElseThrow(()->new RuntimeException("Id not found"));
+       List<Questions> questionsList = quiz.getQuestionsList();
+       List<QuestionDTO> questionDTOS = new ArrayList<>();
+
+       for(Questions q : questionsList){
+           QuestionDTO dto = new QuestionDTO(q.getId(),q.getQuestion(),q.getOption1(),
+                   q.getOption2(),q.getOption3(),q.getOption4());
+           questionDTOS.add(dto);
+       }
+        return questionDTOS;
+    }
+}
