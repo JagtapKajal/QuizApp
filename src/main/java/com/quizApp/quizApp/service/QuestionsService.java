4554f6f3fa3b3ac9679c83e16f94491e69f173ce
jagtapK <jagtapkajal54@gmail.com>
2025-09-10 15:23:34 +0530

    Implemented method to get question list

---
+++ src/main/java/com/quizApp/quizApp/service/QuestionsService.java
@@ -0,0 +1,10 @@
+package com.quizApp.quizApp.service;
+
+import com.quizApp.quizApp.entity.Questions;
+
+import java.util.List;
+
+public interface QuestionsService {
+
+    public List<Questions> getAllQuestions();
+}
