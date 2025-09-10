7d0d32709c0fbcfe7e13caa49322ca1f11213624
jagtapK <jagtapkajal54@gmail.com>
2025-09-10 15:17:35 +0530

    Implemented method to create quiz

---
+++ src/main/java/com/quizApp/quizApp/controller/QuizController.java
@@ -0,0 +1,36 @@
+package com.quizApp.quizApp.controller;
+
+import com.quizApp.quizApp.DTO.QuestionDTO;
+import com.quizApp.quizApp.service.QuizService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class QuizController {
+
+    @Autowired
+    private QuizService quizService;
+
+    // To create Quiz
+    @PostMapping("/CreateQuiz")
+    public ResponseEntity<String> createQuiz(@RequestParam int noOfQuestions,
+                                             @RequestParam String quizname,
+                                             @RequestParam String category){
+
+        String msg = quizService.createQuiz(quizname,noOfQuestions,category);
+        return new ResponseEntity<>(msg , HttpStatus.CREATED);
+    }
+
+    // To get Quiz questions
+    @GetMapping("/getQuiz/{id}")
+    public ResponseEntity<List<QuestionDTO>> getQuizQuestions(@PathVariable("id") int id){
+        List<QuestionDTO> questionlist = quizService.getQuizQuestion(id);
+        return new ResponseEntity<>(questionlist,HttpStatus.OK);
+    }
+
+
+}
