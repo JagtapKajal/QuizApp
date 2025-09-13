4820bc20bd0f3fc6ae20b9a6bddc9f72929ab9a4
jagtapK <jagtapkajal54@gmail.com>
2025-09-13 19:12:34 +0530

    Added quiz request class to get quiz id and options list

---
+++ src/main/java/com/quizApp/quizApp/request/QuizRequest.java
@@ -0,0 +1,24 @@
+package com.quizApp.quizApp.request;
+
+public class QuizRequest {
+
+    private int qid;
+
+    private String optionList;
+
+    public int getQid() {
+        return qid;
+    }
+
+    public void setQid(int qid) {
+        this.qid = qid;
+    }
+
+    public String getOptionList() {
+        return optionList;
+    }
+
+    public void setOptionList(String optionList) {
+        this.optionList = optionList;
+    }
+}
